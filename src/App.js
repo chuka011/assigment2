756e9dd3f08ec381e1dd3669b2a7c9d1e3e33bc4
chuka011 <chuka011@gmail.com>
2018-03-22 20:46:06 +0100

    Dodata komponenta za validaciju

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
 import './App.css';
-
+import ValidationComponent from './ValidationComponent'
 class App extends Component {
+  state = {
+      str: "",
+      numberOfChars: 0
+  };
+
+   charInputHandler = (event) => {
+    const str = event.target.value;
+    this.setState({
+      str: str,
+      numberOfChars: str.length
+    })
+  }
   render() {
     return (
       <div className="App">
-        <input type="text"/>
+        <input type="text" onChange={this.charInputHandler} value={this.state.str}/>
+        <p>Number of chars: {this.state.numberOfChars}</p>
+        <ValidationComponent len={this.state.numberOfChars}/>
       </div>
     );
   }
